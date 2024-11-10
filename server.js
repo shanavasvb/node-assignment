a47fb38e13d610db865b15346315ad5b97ff558e
shanavasvb <shanavasvbasheer@gmail.com>
2024-11-10 21:46:28 +0530

    server.js

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ var fs = require('fs');
 var path = require('path');
 
 http.createServer(function(req, res) {
-    // Determine the requested file path based on the URL
+
     var filePath = path.join(__dirname, req.url === '/' ? 'html/index.html' : req.url);
 
-    // Determine the file extension and set the content type
+    
     var extname = path.extname(filePath).toLowerCase();
     var contentType = 'text/html';
 
     if (extname === '.css') contentType = 'text/css';
     if (extname === '.js') contentType = 'application/javascript';
 
-    // Read the requested file and serve it
+    
     fs.readFile(filePath, function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
